7c3c51e74d32eebe98a50e61d527da3296d95072
bracesproul <46789226+bracesproul@users.noreply.github.com>
2019-05-24 10:05:05 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let input, button, greeting;
 function setup() {
   greeting = createElement('h1','Welcome To Braces Home Page');
-  greeting.position(10, 100);
+  greeting.position(10, 10);
 
 <li> <a href= "bracesproul.github.io/ballgame.js">Ball Game</a></li>
 
