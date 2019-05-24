ea0541379a7a45d2fd28590234e5b484ab3ffee8
bracesproul <46789226+bracesproul@users.noreply.github.com>
2019-05-24 09:38:06 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -58,3 +58,11 @@ function draw() {
   }
 }
 */
+
+let input, button, greeting;
+function setup() {
+  greeting = createElement('h1','Welcome To Braces Home Page');
+  greeting.position(10, 100);
+
+
+}
