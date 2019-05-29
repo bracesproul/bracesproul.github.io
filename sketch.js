e3fb3bddd89943a268ed62c0d8aa8223cbbafafa
bracesproul <46789226+bracesproul@users.noreply.github.com>
2019-05-29 10:01:19 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,22 @@
 let input, button, greeting;
+var col = 0;
 function setup() {
     greeting = createElement('h1','Welcome To Braces Home Page');
-    greeting.position(300, 300);
+    greeting.position(25, 60);
+    createCanvas(innerWidth, innerHeight);
+
+
+}
+
+function draw() {
+
+    col = mouseX / 2;
+    col - mouseY / 2
+    background(col);
+
+
+
+    fill(255, 0, 0);
+    ellipse(mouseX, mouseY, 25, 25, 25);
 
 }
